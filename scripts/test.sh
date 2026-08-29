c2fa03be0afac3e4e4ce8a44f09cfa30062c4be1
Siak Hooi <73884114+siakhooi@users.noreply.github.com>
2026-08-29 07:51:08 +0000

    fix test.sh should stop when fail

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -e -x
+set -euo pipefail
+set -x
 
-go test -v -json -covermode=atomic -coverpkg=./... -coverprofile=test-coverage.out ./... |tee test-report.json
+go test -v -json -covermode=atomic -coverpkg=./... -coverprofile=test-coverage.out ./... | tee test-report.json
 
 go tool cover -func=test-coverage.out
-
 go tool cover -html=test-coverage.out -o test-coverage.html
